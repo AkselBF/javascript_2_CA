b7f5931d3ac7717897a722876ea36a6ae12a1234
AkselBF <aksel.boutrouefaret@yahoo.com>
2023-10-12 15:11:02 +0200

    change script and jwt

---
+++ js/modules/jwt.mjs
@@ -0,0 +1,24 @@
+
+async function getWithToken(url, method = "GET") {
+  try {
+    console.log(url);
+    const token = localStorage.getItem("accessToken");
+    console.log(token);
+    const fetchOptions = {
+      method: method,
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`,
+      }
+    };
+
+    const response = await fetch(url, fetchOptions);
+    console.log(response);
+
+    const json = await response.json();
+    console.log(json);
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
--- js/script.mjs
+++ js/script.mjs
@@ -2,6 +2,7 @@
 
 import { registerUser } from "./modules/auth.mjs";
 import { loginUser } from "./modules/login.mjs";
+import { getWithToken } from "./modules/jwt.mjs";
 //import { fetchData } from "./modules/api.mjs";
 //import { postData } from "./handler/post.mjs";
 //import { updateData } from "./update.mjs";
@@ -18,6 +19,7 @@ else {
 // API url
 const baseUrl = "https://api.noroff.dev/api/v1";
 
+
 // For register function
 const userToRegister = {
   name: "test_user_demo",
@@ -28,6 +30,7 @@ const userToRegister = {
 const registerUrl = `${baseUrl}/social/auth/register`;
 registerUser(registerUrl, userToRegister);
 
+
 // For login function
 const userToLogin = {
   email: "test_user_demo2@noroff.no",
@@ -37,30 +40,7 @@ const userToLogin = {
 const loginUrl = `${baseUrl}/social/auth/login`;
 loginUser(loginUrl, userToLogin);
 
+
 // For JWT token
-async function getWithToken(url, method = "GET") {
-  try {
-    console.log(url);
-    const token = localStorage.getItem("accessToken");
-    console.log(token);
-    const fetchOptions = {
-      method: method,
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${token}`,
-      }
-    };
-
-    const response = await fetch(url, fetchOptions);
-    console.log(response);
-
-    const json = await response.json();
-    console.log(json);
-  }
-  catch (error) {
-    console.log(error);
-  }
-}
-
 const postsUrl = `${baseUrl}/social/posts`;
 getWithToken(postsUrl);
